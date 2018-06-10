9533ace36945b03d59ed9ec7d5fe58ed5bfa8ed1
Ruuttunen Tuomas P <rutu@lx7-fuxi015.ad.helsinki.fi>
2018-06-10 23:36:34 +0300

    rekiströinti mahdollista

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -2,8 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
-    username = StringField("Username", [validators.Length(min=4)]) 
-    password = PasswordField("Password", [validators.Length(min=4)])
+    username = StringField("Username", [validators.DataRequired(), validators.Length(min=4)]) 
+    password = PasswordField("Password", [validators.DataRequired(), validators.Length(min=4)])
   
+    class Meta:
+        csrf = False
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", [validators.DataRequired(), validators.Length(min=1)])
+    username = StringField("Username", [validators.DataRequired(), validators.Length(min=4)]) 
+    password = PasswordField("Password", [validators.DataRequired(), validators.Length(min=4)])
+    
     class Meta:
         csrf = False
