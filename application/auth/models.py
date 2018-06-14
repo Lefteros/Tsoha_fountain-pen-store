e4b3f6cdef98a8b763c6aa2d04bd15551c843b2d
Ruuttunen Tuomas P <rutu@lx7-fuxi015.ad.helsinki.fi>
2018-06-14 15:05:58 +0300

    toiminto käyttäjien listaukselle ja kerrotaan kuinka monta kynää löytyy kokoelmasta (yhteenvetokyselyllä)

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,4 +1,5 @@
 from application import db
+from sqlalchemy.sql import text
 
 class User(db.Model):
 
@@ -31,3 +32,35 @@ class User(db.Model):
 
     def is_authenticated(self):
         return True
+
+    @staticmethod
+    def get_users_and_pennro():
+
+        stmt1 = text("SELECT account.name FROM account, collection"
+                    " WHERE account.id = collection.account_id")
+        res1 = db.engine.execute(stmt1)
+        count = []
+        for row in res1:
+            count.append({row[0]})
+
+        stmt2 = text("SELECT account.name FROM account")
+        res2 = db.engine.execute(stmt2)
+        names = []
+        for row in res2:
+            names.append({row[0]})
+        
+        response = []
+        i = 0
+        for value in names:
+            nro = 0
+            for name in count:
+                if name == value:
+                    nro = nro + 1
+  
+            S = str(value)
+            length = len(S)
+            S = S[7:length-3]
+            response.append({"name":S, "nro":nro})    
+ 
+
+        return response
